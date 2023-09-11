08b025b1486216e04434d69104945dd45420acb4
Chris <chris.r.weng@gmail.com>
2023-09-11 02:04:33 -0700

    Index add method
    Also fixed the constructor to create the Index file and the folder is now also "objects"

--- Index.java
+++ Index.java
@@ -1,20 +1,30 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class Index {
     public Index() {
-        String folderName = "indexFolder";
+        String folderName = "objects";
         File folder = new File(folderName);
         if (!folder.exists()) {
             folder.mkdirs();
         }
+
+        File file = new File("Index");
     }
 
-    public static void add(File fileName) {
+    public static void add(String fileName) throws FileNotFoundException {
+        Blob blob = new Blob(fileName);
+        File file = new File("Index");
 
+        String content = Blob.fileToString(new File(fileName));
+        String hash = Blob.hashString(content);
+        PrintWriter pw = new PrintWriter(file);
+        pw.print(fileName + " " + hash + "\n");
+        pw.close();
     }
 
-    public static void remove(File fileName) {
+    public static void remove(String fileName) {
 
     }
 }
